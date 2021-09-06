596b17e2d9856daeccf073af87cb794521c79fba
cskmacedo <kleber.barreto@msn.com>
2021-09-06 10:25:10 -0300

    repositories finalizados (faltam alguns ajustes futuros)

---
+++ Back_Java/sisunico/src/main/java/br/com/kmacedo/sisunico/repository/MycarHistoricoManutencaoRepository.java
@@ -0,0 +1,15 @@
+package br.com.kmacedo.sisunico.repository;
+
+
+import br.com.kmacedo.sisunico.model.MycarHistoricoManutencaoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.sql.Date;
+import java.util.List;
+
+public interface MycarHistoricoManutencaoRepository extends JpaRepository<MycarHistoricoManutencaoModel, Long> {
+
+    @Query(value = "select h from MycarHistoricoManutencaoModel h")
+    List<MycarHistoricoManutencaoModel> buscarPorData(Long idhistoricomanutencao);
+}
