f5f0a5946a033e28c2e1ce8f42a93409666d18d8
cskmacedo <kleber.barreto@msn.com>
2021-09-12 00:39:31 -0300

    escrita das querys com inner join

--- Back_Java/sisunico/src/main/java/br/com/kmacedo/sisunico/repository/MycarHistoricoManutencaoRepository.java
+++ Back_Java/sisunico/src/main/java/br/com/kmacedo/sisunico/repository/MycarHistoricoManutencaoRepository.java
@@ -1,6 +1,7 @@
 package br.com.kmacedo.sisunico.repository;
 
 
+import br.com.kmacedo.sisunico.model.MycarCompraModel;
 import br.com.kmacedo.sisunico.model.MycarHistoricoManutencaoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,4 +14,14 @@ public interface MycarHistoricoManutencaoRepository extends JpaRepository<MycarH
     //TODO -> Ajustar a query Historico de Manutencao
     @Query(value = "select h from MycarHistoricoManutencaoModel h")
     List<MycarHistoricoManutencaoModel> buscarPorData(Long idhistoricomanutencao);
+
+    @Query(value = "select hist.idhistoricomanutencao, hist.km, hist.datamanutencao, hist.kmproximatroca, " +
+            "hist.valor, fab.fabricante, tpm.tipomanutencao, comp.componente, v.veiculodescricao " +
+            "from MycarHistoricoManutencaoModel hist" +
+            "inner join hist.componente comp" +
+            "inner join hist.fabricante fab" +
+            "inner join hist.tipomanutencao tpm" +
+            "inner join hist.veiculodescricao v" +
+            "order by hist.idhistoricomanutencao desc")
+    List<MycarHistoricoManutencaoModel> exibirHistoricoManutencao(String historicomanutencao);
 }
